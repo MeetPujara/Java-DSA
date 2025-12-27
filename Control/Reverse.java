f0d169b314ab4bade8577f91d5790a2c6b82eb7c
MeetPujara <meetpujara02@gmail.com>
2025-12-27 22:47:41 +0530

    Reverse Prg Done

---
+++ Control/Reverse.java
@@ -0,0 +1,19 @@
+package Control;
+
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = in.nextInt();
+        int ans = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            num /= 10;
+            ans = ans * 10 + rem;
+        }
+        System.out.println(ans);
+        in.close();
+    }
+}
