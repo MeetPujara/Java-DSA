11e7054150d6c76ef7b586c25fc3fb4d4c3c07e5
MeetPujara <meetpujara02@gmail.com>
2025-12-28 23:30:14 +0530

    switch case

---
+++ Switch_Statements/switch_stat.java
@@ -0,0 +1,11 @@
+package Switch_Statements;
+
+import java.util.Scanner;
+
+public class switch_stat {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        in.close();
+    }
+}
