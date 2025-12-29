bdd578de9a001171971816296d2206c5eb88f0e6
MeetPujara <meetpujara02@gmail.com>
2025-12-29 23:01:44 +0530

    Describe Fruit Prg done

--- Switch_Statements/switch_stat.java
+++ Switch_Statements/switch_stat.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class switch_stat {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String fruit = in.next();
+
+        switch (fruit) {
+            case "apple" -> System.out.println("It is apple");
+            case "mango" -> System.out.println("It is Mango");
+        }
 
         in.close();
     }
