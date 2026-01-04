10e2dc20804e73dbcbb9bf004ca59e2db037ddf3
MeetPujara <meetpujara02@gmail.com>
2026-01-04 23:20:21 +0530

    Area_Of_Rectangle Prg Done

---
+++ Assignments/Conditions_Loops/Basics/Area_Of_Rectangle.java
@@ -0,0 +1,11 @@
+package Assignments.Conditions_Loops.Basics;
+
+public class Area_Of_Rectangle {
+    static void main() {
+        int length = 4;
+        int width = 3;
+
+        int area_of_rectangle = length * width;
+        System.out.println(area_of_rectangle);
+    }
+}
