4a59a1b1a22d0c1f472d04eb05eb8fe3b9c8f1eb
MeetPujara <meetpujara02@gmail.com>
2026-01-04 23:19:59 +0530

    Area_Of_IsocelesTriangle Prg Done

---
+++ Assignments/Conditions_Loops/Basics/Area_Of_IsocelesTriangle.java
@@ -0,0 +1,11 @@
+package Assignments.Conditions_Loops.Basics;
+
+public class Area_Of_IsocelesTriangle {
+    static void main() {
+        int base = 4;
+        int height = 3;
+
+        float area_of_triangle = 0.5f * base * height;
+        System.out.println(area_of_triangle);
+    }
+}
