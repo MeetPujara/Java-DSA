64af2c41bf423280ba554a6ff59eeb94dc4db6eb
MeetPujara <meetpujara02@gmail.com>
2026-01-04 22:50:35 +0530

    Area of Circle Prg Done

---
+++ Assignments/Conditions_Loops/Basics/Area_Of_Circle.java
@@ -0,0 +1,12 @@
+package Assignments.Conditions_Loops.Basics;
+
+public class Area_Of_Circle {
+    static void main() {
+        int r = 5;
+        float pi = 3.14f;
+
+        float area_of_circle = pi * r * r;
+        System.out.println(area_of_circle);
+
+    }
+}
