802bf7666e896b359b978dde6a3abb02cf20eabe
MeetPujara <meetpujara02@gmail.com>
2026-01-05 08:32:41 +0530

    Area Of Parallelogram Prg Done

---
+++ Assignments/Conditions_Loops/Basics/Area_Of_Parallelogram.java
@@ -0,0 +1,11 @@
+package Assignments.Conditions_Loops.Basics;
+
+public class Area_Of_Parallelogram {
+    static void main() {
+        int height = 4;
+        int base = 3;
+
+        int area_of_parallelogram = height * base;
+        System.out.println(area_of_parallelogram);
+    }
+}
