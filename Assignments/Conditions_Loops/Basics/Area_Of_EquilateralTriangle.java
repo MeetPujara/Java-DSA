2d3745d27c5bcebacf1025f6929794bf88a9a444
MeetPujara <meetpujara02@gmail.com>
2026-01-06 23:34:34 +0530

    Area_Of_EquilateralTriangle Prg Done

---
+++ Assignments/Conditions_Loops/Basics/Area_Of_EquilateralTriangle.java
@@ -0,0 +1,11 @@
+package Assignments.Conditions_Loops.Basics;
+
+public class Area_Of_EquilateralTriangle {
+    static void main() {
+        float num = (float) (Math.sqrt(3) / 4);
+        int side = 4 * 4;
+
+        float area_of_et = num * side;
+        System.out.println(area_of_et);
+    }
+}
