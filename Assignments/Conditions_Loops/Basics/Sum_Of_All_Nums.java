fc9385223323e7ffd2ac5a422b1f702254045348
MeetPujara <meetpujara02@gmail.com>
2026-01-07 21:28:38 +0530

    Sum Of All Number Prg Done

--- Assignments/Conditions_Loops/Basics/Sum_Of_All_Nums.java
+++ Assignments/Conditions_Loops/Basics/Sum_Of_All_Nums.java
@@ -6,11 +6,11 @@ public class Sum_Of_All_Nums {
     static void main() {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Enter a Number: ");
-        int n = in.nextInt();
         int sum = 0;
 
         while (true){
+            System.out.print("Enter a Number: ");
+            int n = in.nextInt();
             if (n == 0){
                 break;
             }else{
