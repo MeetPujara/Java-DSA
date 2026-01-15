79cc00c75e215aa51ed0450bbd95f35885240269
MeetPujara <meetpujara02@gmail.com>
2026-01-15 19:22:58 +0530

    Create and Print the array

---
+++ Array/Array_Prg.java
@@ -0,0 +1,11 @@
+package Array;
+
+import java.util.Arrays;
+
+public class Array_Prg {
+    static void main() {
+//        int[] rno = new int[5];
+        int[] rnos = {1,2,3,4,5};
+        System.out.println(Arrays.toString(rnos));
+    }
+}
