7e55f1d89f7e35af86b52e5c045bd8b8e16191c0
MeetPujara <meetpujara02@gmail.com>
2026-01-18 23:10:13 +0530

    Reverse an Array

---
+++ Array/Reverse_Array.java
@@ -0,0 +1,34 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Reverse_Array {
+    static void main() {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter value for index " + i + ": ");
+            arr[i] = in.nextInt();
+        }
+        rev(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void rev(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start<end){
+                swap(arr,start,end);
+                start++;
+                end--;
+        }
+    }
+
+    static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
