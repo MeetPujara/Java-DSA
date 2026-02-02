fd2646cfa802aa45eee34aea69dba532bccdbcb4
MeetPujara <meetpujara02@gmail.com>
2026-02-02 23:25:16 +0530

    Matrix_Diagonal_Sum

---
+++ Assignments/Array/Matrix_Diagonal_Sum.java
@@ -0,0 +1,15 @@
+package Assignments.Array;
+
+public class Matrix_Diagonal_Sum {
+    static void main() {
+        int[][] mat = {{1,2,3},{4,5,6},{7,8,9}};
+        int sum = 0;
+        int n = mat.length;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < i; j++) {
+
+            }
+        }
+        System.out.println(sum);
+    }
+}
