bfab5f241df7428e737c56986e21cb594a97f713
MeetPujara <meetpujara02@gmail.com>
2026-02-17 20:56:03 +0530

    Cells_With_Odd_Values

---
+++ Assignments/Array/Cells_With_Odd_Values.java
@@ -0,0 +1,18 @@
+package Assignments.Array;
+
+public class Cells_With_Odd_Values {
+    static void main() {
+        int m = 2;
+        int n = 3;
+        int [][] ans = new int[m][n];
+        int [][] indices = {{0,1},{1,1}};
+        int counter = 0;
+
+        for (int i = 0; i < indices.length; i++) {
+            for (int j = 0; j < n; j++) {
+                
+            }
+        }
+        
+    }
+}
