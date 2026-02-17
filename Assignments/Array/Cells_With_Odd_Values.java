0614f3ca0448365e0818b594de8009a6d15d7e8d
MeetPujara <meetpujara02@gmail.com>
2026-02-17 20:57:43 +0530

    Cells_With_Odd_Values

--- Assignments/Array/Cells_With_Odd_Values.java
+++ Assignments/Array/Cells_With_Odd_Values.java
@@ -10,7 +10,7 @@ public class Cells_With_Odd_Values {
 
         for (int i = 0; i < indices.length; i++) {
             for (int j = 0; j < n; j++) {
-                
+                System.out.println();
             }
         }
         
