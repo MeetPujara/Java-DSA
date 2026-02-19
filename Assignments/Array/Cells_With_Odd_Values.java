cb20b62b865c83f9e6f897610ff44ea3538db3d3
MeetPujara <meetpujara02@gmail.com>
2026-02-19 18:07:00 +0530

    Cells_With_Odd_Values

--- Assignments/Array/Cells_With_Odd_Values.java
+++ Assignments/Array/Cells_With_Odd_Values.java
@@ -9,11 +9,23 @@ public class Cells_With_Odd_Values {
         int counter = 0;
 
         for (int i = 0; i < indices.length; i++) {
+            //row is constant
             for (int j = 0; j < n; j++) {
-                System.out.println();
-                System.out.println();
+                ans[indices[i][0]][j]++;
+            }
+            //column is constant
+            for (int k = 0; k < m; k++) {
+                ans[k][indices[i][1]]++;
             }
         }
-        
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(ans[i][j] %2 != 0){
+                    counter++;
+                }
+            }
+        }
+        System.out.println(counter);
     }
 }
