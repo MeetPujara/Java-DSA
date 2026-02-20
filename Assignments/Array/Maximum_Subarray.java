4ea3e367aaadea835b0bb237a3a600492bef65e9
MeetPujara <meetpujara02@gmail.com>
2026-02-20 14:19:16 +0530

    Maximum_Subarray

---
+++ Assignments/Array/Maximum_Subarray.java
@@ -0,0 +1,18 @@
+package Assignments.Array;
+
+public class Maximum_Subarray {
+    static void main() {
+        int[] nums = {5,4,-1,7,8};
+        int n = nums.length;
+        int max = Integer.MIN_VALUE, sum = 0;
+
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            max = Math.max(sum,max);
+
+            if(sum<0) sum = 0;
+        }
+
+        System.out.println(max);
+    }
+}
