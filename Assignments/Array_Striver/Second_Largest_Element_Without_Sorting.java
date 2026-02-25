e90d6c51a55a83fd5e37702a55858f3bd910f635
MeetPujara <meetpujara02@gmail.com>
2026-02-25 17:05:20 +0530

    Second_Largest_Element_Without_Sorting

---
+++ Assignments/Array_Striver/Second_Largest_Element_Without_Sorting.java
@@ -0,0 +1,21 @@
+package Assignments.Array_Striver;
+
+public class Second_Largest_Element_Without_Sorting {
+    static void main() {
+        int[] nums = {9,8,17,16,5};
+        int largest = nums[0];
+        int second_largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] > largest){
+                largest = nums[i];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] > second_largest && nums[i] != largest){
+                second_largest = nums[i];
+            }
+        }
+        System.out.println(second_largest);
+    }
+}
