34500bb09b360706bb63796fa5a4cda4ce5671b5
MeetPujara <meetpujara02@gmail.com>
2026-03-11 00:02:15 +0530

    Move_Zeros_To_End

--- Assignments/Array_Striver/Move_Zeros_To_End.java
+++ Assignments/Array_Striver/Move_Zeros_To_End.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class Move_Zeros_To_End {
     static void main() {
         int[] arr = {0,1,4,0,5,2};
-        int start = arr[0];
-        int end = arr.length-1;
-        for (int i = 0; i < arr.length; i++) {
-            if(start!=end){
-                end = start;
-                start++;
-                end--;
+        int left = 0;
+        for (int right = 0; right < arr.length; right++) {
+            if(arr[right] != 0){
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
             }
         }
         System.out.println(Arrays.toString(arr));
