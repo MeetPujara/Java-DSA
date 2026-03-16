e9883c58b8cb2f25fd6e29ea100b81c9094e5497
MeetPujara <meetpujara02@gmail.com>
2026-03-16 22:44:23 +0530

    P1

---
+++ Patterns/P1.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class P1 {
+    static void main() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
