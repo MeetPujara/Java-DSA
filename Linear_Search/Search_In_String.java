c495386e4dfc1191584fdcc6f7edf94f17e68d87
MeetPujara <meetpujara02@gmail.com>
2026-03-19 16:57:43 +0530

    Search_In_String

---
+++ Linear_Search/Search_In_String.java
@@ -0,0 +1,31 @@
+package Linear_Search;
+
+public class Search_In_String {
+    void main() {
+        System.out.println(search("Meet",'m'));
+        System.out.println(search2("Meet",'m'));
+    }
+
+    static boolean search(String str, char target){
+        if(str.length() == 0){
+            return false;
+        }
+        for(int i = 0; i<str.length(); i++){
+            if(target == str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+    static boolean search2(String str, char target){
+        if(str.length() == 0){
+            return false;
+        }
+        for(char ch: str.toCharArray()){
+            if(ch == target){
+                return false;
+            }
+        }
+        return false;
+    }
+}
