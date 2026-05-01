54962c3defd153466f4b0ecf32aa9f4cfeeb5c91
MeetPujara <meetpujara02@gmail.com>
2026-05-01 21:32:17 +0530

    Search in RowColMatrix

---
+++ Binary_Search_2D/RowColMatrix.java
@@ -0,0 +1,31 @@
+package Binary_Search_2D;
+
+import java.util.Arrays;
+
+public class RowColMatrix {
+    static void main() {
+        int[][] arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        System.out.println(Arrays.toString(search(arr, 8)));
+    }
+
+    static int[] search(int[][]matrix, int target) {
+        int row = 0;
+        int col = matrix.length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return new int[]{row, col};
+            }
+            if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
