3c7bf44c1f8797ab0e246801594e7511fdda3c61
MeetPujara <meetpujara02@gmail.com>
2026-07-18 12:48:09 +0530

    LinearSearch

--- Recursion/LinearSearch.java
+++ Recursion/LinearSearch.java
@@ -1,15 +1,21 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class LinearSearch {
     static void main() {
-        int[] arr = {1,2,3,4,5,6,7};
-        int target = 5;
-        System.out.println(find(arr,target,0));
-        System.out.println(findIndex(arr,target,0));
+        int[] arr = {1,2,3,4,5,6,6,7};
+        int target = 6;
+//        System.out.println(find(arr,target,0));
+//        System.out.println(findIndex(arr,target,0));
+//        findAllIndex(arr,target,0);
+//        System.out.println(li);
+        System.out.println(findAllIndexwithList(arr,target,0,new ArrayList<>()));
     }
 
     static boolean find(int[] arr,int target,int index){
-        if(index == arr.length - 1){
+        if(index == arr.length){
             return false;
         }
         if (arr[index] == target){
@@ -19,12 +25,33 @@ public class LinearSearch {
     }
 
     static int findIndex(int[] arr,int target,int index){
-        if(index == arr.length-1){
+        if(index == arr.length){
             return -1;
         }
         if(arr[index] == target){
             return index;
         }
         return findIndex(arr,target,index+1);
+    }
+
+    static ArrayList<Integer> li = new ArrayList<>();
+    static void findAllIndex(int[]arr,int target,int index){
+        if(index == arr.length){
+            return;
+        }
+        if(arr[index] == target){
+            li.add(index);
+        }
+        findAllIndex(arr,target,index+1);
+    }
+
+    static ArrayList findAllIndexwithList(int[]arr,int target,int index,ArrayList<Integer> listt){
+        if(index == arr.length){
+            return listt;
+        }
+        if(arr[index] == target){
+            listt.add(index);
+        }
+        return findAllIndexwithList(arr,target,index+1,listt);
     }
 }
