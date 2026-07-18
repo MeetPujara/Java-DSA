cad10ff3b43a83587d9fa98c217f5a9350afae81
MeetPujara <meetpujara02@gmail.com>
2026-07-18 12:19:19 +0530

    LinearSearch

--- Recursion/LinearSearch.java
+++ Recursion/LinearSearch.java
@@ -5,6 +5,7 @@ public class LinearSearch {
         int[] arr = {1,2,3,4,5,6,7};
         int target = 5;
         System.out.println(find(arr,target,0));
+        System.out.println(findIndex(arr,target,0));
     }
 
     static boolean find(int[] arr,int target,int index){
@@ -14,6 +15,16 @@ public class LinearSearch {
         if (arr[index] == target){
             return true;
         }
-        return arr[index] != target && find(arr,target,index+1);
+        return find(arr,target,index+1);
+    }
+
+    static int findIndex(int[] arr,int target,int index){
+        if(index == arr.length-1){
+            return -1;
+        }
+        if(arr[index] == target){
+            return index;
+        }
+        return findIndex(arr,target,index+1);
     }
 }
