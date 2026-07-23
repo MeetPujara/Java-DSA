0f6b7818581e3bf108f637f5d76460f837e53fa1
MeetPujara <meetpujara02@gmail.com>
2026-07-23 15:39:46 +0530

    MergeSort

--- MergeSort/Merge_Sort.java
+++ MergeSort/Merge_Sort.java
@@ -4,47 +4,99 @@ import java.util.Arrays;
 
 public class Merge_Sort {
     static void main() {
-        int[] arr = {1,4,5,7,29,2,3};
+        int[] arr = {1, 4, 5, 7, 29, 2, 3};
         arr = sort(arr);
+        System.out.println(Arrays.toString(arr));
+        mergeSortInPlace(arr,0,arr.length);
         System.out.println(Arrays.toString(arr));
     }
 
-    static int[] sort(int[] arr){
-        if(arr.length == 1){
+    static int[] sort(int[] arr) {
+        if (arr.length == 1) {
             return arr;
         }
         int mid = arr.length / 2;
-        int[] left = sort(Arrays.copyOfRange(arr,0,mid));
-        int[] right = sort(Arrays.copyOfRange(arr,mid,arr.length));
-        return merge(left,right);
+        int[] left = sort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = sort(Arrays.copyOfRange(arr, mid, arr.length));
+        return merge(left, right);
     }
 
-    static int[] merge(int[] left,int[] right){
+    static int[] merge(int[] left, int[] right) {
         int[] mix = new int[left.length + right.length];
         int i = 0;
         int j = 0;
         int k = 0;
 
-         while(i < left.length && j < right.length){
-             if(left[i] < right[j]){
-                 mix[k] = left[i];
-                 i++;
-             }else{
-                 mix[k] = right[j];
-                 j++;
-             }
-             k++;
-         }
-        while(i<left.length){
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                mix[k] = left[i];
+                i++;
+            } else {
+                mix[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < left.length) {
             mix[k] = left[i];
             i++;
             k++;
         }
-        while(j<right.length){
+        while (j < right.length) {
             mix[k] = right[j];
             j++;
             k++;
         }
         return mix;
+    }
+
+    static void mergeSortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+
+        int mid = (s + e) / 2;
+
+        mergeSortInPlace(arr, s, mid);
+        mergeSortInPlace(arr, mid, e);
+
+        mergeInPlace(arr, s, mid, e);
+    }
+
+    private static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] mix = new int[e - s];
+
+        int i = s;
+        int j = m;
+        int k = 0;
+
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while (i < m) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int l = 0; l < mix.length; l++) {
+            arr[s + l] = mix[l];
+        }
     }
 }
